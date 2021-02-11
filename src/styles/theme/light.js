b3ca4f9bfd77799630c0f389bb188f9c016737aa
Christian Possidonio <christian@orientme.com.br>
2021-02-11 02:06:20 -0300

    Estilização base do layout!

---
+++ src/styles/theme/light.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#3772FF',
+    secondary: '#FDCA40',
+    background: '#f3f2ef',
+    text: '#333',
+    topBar: '#DF2935',
+  },
+};
